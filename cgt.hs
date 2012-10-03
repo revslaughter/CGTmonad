a2be3043cd27b15658c4aaf8aa0dde9f0ef8ef89
libra guest <prairie.squidman@gmail.com>
2012-10-03 10:47:59 -0400

    See if this fixes showGame

--- cgt.hs
+++ cgt.hs
@@ -3,7 +3,7 @@
 
 data Game = Position [Game] [Game]
 
-zero = Position [] []
+zeroGame = Position [] []
 
 putL :: Game -> Game -> Game
 putL goLeft (Position left right) = Position (goLeft:left) right
@@ -13,7 +13,10 @@ putR goRight (Position left right) = Position left (goRight:right)
 
 showGame :: Game -> String
 showGame (Position [] []) = "{|}"
-showGame (Position (l:ls) (r:rs))
-    = "{"++(showGame l) ++"|"++(showGame r)++"}"
+showGame (Position left []) = "{"++(showGame l <- left)++"|}"
+showGame (Position [] right) ="{|"++(showGame r <- right)++"}"
+showGame (Position left right)
+    = "{"++(showGame l <- left)++"|"++(showGame r <- right)++"}"
+
 
 --fun!
